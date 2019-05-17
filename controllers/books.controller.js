430b4e587149718eb2d779866a887510402922c0
SandraGro <sandra.guerreroflores@gmail.com>
2019-05-17 12:07:52 -0500

    se agregó endpoint para crear libro

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -6,14 +6,41 @@ const BooksController = {
       .find()
       .then(data => {
         if (data.length === 0) {
-          res.status(404).send({ message: "Todavía no hay libros!" })
+          res.status(404).send({ message: "Todavia no hay libros!" })
         } else {
           res.status(200).send(data)
         }
       })
       .catch(err => {
-        res.status(500).send({ message: "Tronó la base de datos! D:" })
+        res.status(500).send({ message: "Trono la Base de Datos! D:" })
       });
+
+  },
+
+  // Crear un libro
+  createBook: (req, res) => {
+    if (!(req.body.title && req.body.author && req.body.pageNumber)) {
+      res.status(400).send({ message: "Necesitas enviar título, autor y número de página" });
+    } else {
+      const title = req.body.title;
+      const author = req.body.author;
+      const pageNumber = req.body.pageNumber;
+      BookModel
+        .create({
+          title,
+          author,
+          pageNumber
+        })
+        .then(data => {
+          // codigo para cuando se crea el libro
+          res.status(200).send({ data: data }).end();
+
+        })
+        .catch(err => {
+          // codigo para cuando falla la creacion del libro o la peticion a la base de datos
+          res.status(500).send({ error: "Falló la creación del libro" });
+        })
+    }
   }
 };
 
