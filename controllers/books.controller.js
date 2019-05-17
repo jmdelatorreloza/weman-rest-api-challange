b4f656d2dd1ca2727e326ca95584f1ba4e9fae6f
viridiana hernandez <vbhg20@gmail.com>
2019-05-17 10:39:28 -0700

    hasta userpoint de creación de libro

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,9 +1,47 @@
 const BookModel = require('../models/books.model');
+const bodyParser = require("body-parser");
 
 const BooksController = {
   readBooks: (req, res) => {
-    res.status(200).send({message: "test"}) 
+    BookModel
+    .find()
+    .then( data => {
+      if (data.length === 0) {
+        res.status(404).send({message: "Todavia no hay libros!"});
+      } else {
+        res.status(200).send(data)
+      }
+    })
+    .catch( err => {
+      res.status(500).send({message:"Trono todo en la base de datos :("})
+    })
+  },
+  createBooks: (req, res) => {
+    if (req.body.title && req.body.author && req.body.pageNumber) {
+      const title = req.body.title;
+      const author = req.body.author;
+      const pageNumber = req.body.pageNumber;
+      BookModel
+      .create({
+        title,
+        author,
+        pageNumber
+      })
+      .then( data => {
+        res.status(200).send({datos:data}).end();
+      })
+      .catch( err => {
+        res.status(500).send({message:"Uy, ya fue :/"}).end();
+      });
+    } else {
+      res.status(400).send({message:"No se puede, manda Title, author y pageNumber"}).end();
+      //console.log(req.body); para revisar que trae el req body
+    }
+  },
+  readBook: (req, res) => {
+    res.end("Holi");
   }
+
 };
 
 module.exports = BooksController;
--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 const db = require('./db');
 const booksController = require('./controllers/books.controller');
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
 app.get('/books', booksController.readBooks);
+app.post("/books", booksController.createBooks);
+app.get("/book", booksController.readBook);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
