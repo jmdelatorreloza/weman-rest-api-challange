dbc04d022cee4894ad53fa9b14e9512961334a70
viri1994 <vdlre1994@gmail.com>
2019-05-17 12:41:41 -0500

    Crear libro

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -14,7 +14,33 @@ const BooksController = {
     .catch( err => {
       res.status(500).send({message: "Trono la Base de Datos! D:"})
     });
+  },
+  createBooks:(req, res)=> {
+    if(req.body.title && req.body.author && req.body.pageNumber){
+      const title = req.body.title;
+      const author = req.body.author;
+      const pageNumber = req.body.pageNumber;
+    BookModel
+    .create({
+    title,
+    author,
+    pageNumber
+    })
+
+.then( data => {
+  res.status(201).send({data: data}).end()
+  })
+
+.catch( err => {
+  res.status(500).send({message: "Datos erroneos"}).end()
+})
+  }else{
+  res.status(404).send({message: "manda title, author y pageNumber para crear un libro"});
+    }
   }
 };
+  // codigo para cuando falla la creacion del libro o la peticion a la base de datos
+
+
 
 module.exports = BooksController;
