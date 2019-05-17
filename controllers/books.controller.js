1346300f3b5ea6c915c4a8ec41285cc5941aacbd
Raquel Saavedra <raquel_2496@hotmail.com>
2019-05-17 10:40:22 -0500

    Leer libros

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -2,7 +2,18 @@ const BookModel = require('../models/books.model');
 
 const BooksController = {
   readBooks: (req, res) => {
-    res.status(200).send({message: "test"}) 
+    BookModel
+    .find()
+    .then( data => {
+      if(data.length === 0) {
+        res.status(404).send({message: "Todavia no hay libros!"})
+      } else {
+        res.status(200).send(data)
+      }
+    })
+    .catch( err => {
+      res.status(500).send({message: "Trono la Base de Datos! D:"})
+    });
   }
 };
 
