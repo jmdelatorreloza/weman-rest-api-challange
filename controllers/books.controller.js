ca0468ade69888caaa9e57b86bba6dee44c52095
estefania11 <este_f_y@hotmail.com>
2019-05-17 12:09:49 -0500

    parte dos

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -14,7 +14,30 @@ const BooksController = {
     .catch( err => {
       res.status(500).send({message: "Trono la Base de Datos! D:"})
     });
+  },
+
+  createBooks: (req, res) => {
+    if(req.body.title && req.body.author && req.body.pageNumber){
+      const title = req.body.title;
+      const author = req.body.author;
+      const pageNumber = req.body.pageNumber;
+      BookModel
+      .create({
+        title,
+        author,
+        pageNumber
+      })
+    .then( data => {
+      res.status(201).send({data : data}).end()
+    })
+    .catch( err => {
+      res.status(500).send({messagge: "Ya valió"})
+    })
+    } else {
+      res.status(400).send({messagge: "Está toda la información"})
+      }
   }
 };
+        
 
 module.exports = BooksController;
