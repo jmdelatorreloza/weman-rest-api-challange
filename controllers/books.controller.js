32478f1b2accb1036f99b4a24df9953c00f82883
Raquel Saavedra <raquel_2496@hotmail.com>
2019-05-17 13:11:34 -0500

    Borrar libro

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -62,25 +62,37 @@ const BooksController = {
     if (!(req.body.title && req.body.author && req.body.pageNumber && req.body.id)) {
       res.status(400).send({ Error: 'Ingresa todos los parametros!' });
     } else {
-    const title = req.body.title;
-    const author = req.body.author;
-    const pageNumber = req.body.pageNumber;
-    const id = req.body.id;
-    BookModel
-      .findOneAndUpdate({
-        _id: id
-      }, {
-          title,
-          author,
-          pageNumber
+      const title = req.body.title;
+      const author = req.body.author;
+      const pageNumber = req.body.pageNumber;
+      const id = req.body.id;
+      BookModel
+        .findOneAndUpdate({
+          _id: id
+        }, {
+            title,
+            author,
+            pageNumber
+          })
+        .then(data => {
+          res.status(201).send({ data: data }).end(); //regresa los datos del libro que se esta sobreescribiendo
         })
+        .catch(err => {
+          res.status(500).send({ message: "Trono la Base de Datos! D:" });
+        })
+    }
+  },
+
+  deleteBook: (req, res) => {
+    let id = req.body.id;
+    BookModel
+      .deleteOne({ _id: id })
       .then(data => {
-        res.status(201).send({ data: data }).end(); //regresa los datos del libro que se esta sobreescribiendo
+        res.status(200).send({ message: "Libro borrado!" }); //manda mensaje de borrado y el libro que se borro
       })
       .catch(err => {
         res.status(500).send({ message: "Trono la Base de Datos! D:" });
-      })
-    }
+      });
   }
 
 }
--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const port = 3000
 const db = require('./db');
 const booksController = require('./controllers/books.controller');
 
-app.use(bodyParser());
+app.use(bodyParser.json());
 
 app.get('/books', booksController.readBooks);
 app.post('/books', booksController.createBook);
 app.get('/books/find', booksController.readOneBook);
 app.put('/books', booksController.updateBook);
+app.delete('/books', booksController.deleteBook);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))   
