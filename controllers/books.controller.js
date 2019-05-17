b5eb1227f87a9eb9d1f611616fb7436ef88ad772
Brenda Michelle <bmichelle.psic@gmail.com>
2019-05-17 12:26:54 -0500

    punto 2

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -14,7 +14,28 @@ const BooksController = {
     .catch( err => {
       res.status(500).send({message: "Trono la Base de Datos! D:"})
     });
-  }
+  },
+  createBook: (req, res) => {
+    if (req.body.title && req.body.author && req.body.pageNumber) {
+        const title = req.body.title;
+        const author = req.body.author;
+        const pageNumber = req.body.pageNumber;
+        BookModel
+        .create ({
+          title,
+          author,
+          pageNumber
+        })  
+        .then(data => {
+          res.status(200).send({data: data}).end();
+        })
+        .catch(err => {
+          res.status(500).send({message: err})
+        })
+    } else {
+      res.status(400).send({message:"Asegurate de incluir titulo, autor y numero de pagina"})
+    }
+  } 
 };
 
 module.exports = BooksController;
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 const db = require('./db');
 const booksController = require('./controllers/books.controller');
 
+app.use(bodyParser())
+
 app.get('/books', booksController.readBooks);
+app.post ('/books', booksController.createBook)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
