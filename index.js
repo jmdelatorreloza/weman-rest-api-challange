3e5154d13bfe39fc66d22c06e1d87f9422a73304
Raquel Saavedra <raquel_2496@hotmail.com>
2019-05-17 14:31:23 -0500

    WIP , middleware verificando token

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = require('./db');
 const booksController = require('./controllers/books.controller');
 const authController = require('./controllers/authentication.controller');
 
+
 app.use(bodyParser.json());
 
 app.get('/books', booksController.readBooks);
@@ -13,6 +14,6 @@ app.post('/books', booksController.createBook);
 app.get('/books/find', booksController.readOneBook);
 app.put('/books', booksController.updateBook);
 app.delete('/books', booksController.deleteBook);
-app.post('/auth', authController.authenticate);
+app.post('/books/auth', authController.authenticate);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))   
